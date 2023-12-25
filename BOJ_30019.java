e43c11ef78b633fceaaf92eb87a58d1416ebc3af
Eunjeong20 <102710280+Eunjeong20@users.noreply.github.com>
2023-12-25 17:13:32 +0900

    강의실 예약 시스템 3rd
    Edit class name

--- BOJ_30019.java
+++ BOJ_30019.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
-public class Main
+public class BOJ_30019
 {
     public static void main(String[] args) throws IOException {
         
